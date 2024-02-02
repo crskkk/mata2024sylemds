fc1ae66e99ac3961e980645a0e8bcbc15e895a0a
Cris <duqueorange+github@gmail.com>
2024-02-01 23:46:38 -0500

    Auth: Getting there

--- src/index.js
+++ src/index.js
@@ -1,2 +1,57 @@
 import { firebaseinit } from './firebasecreds.js';
-firebase();
+firebaseinit();
+
+const email = document.querySelector('#email')
+const password = document.querySelector('#password')
+const signUpButton = document.querySelector('#signUpButton')
+const signInButton = document.querySelector('#signInButton')
+
+// Sign Up
+signUpButton.addEventListener('click', ()=>{
+    const aemail = email.value
+    const apassword = password.value
+    createUserWithEmailAndPassword(auth,aemail,apassword)
+    .then((cred)=>{
+      console.log(cred.user)
+      // RESUME: Hide\Show\Sh
+      document.getElementById("email").remove()
+      document.getElementById("password").remove()
+      document.getElementById("signUpButton").remove()
+      document.getElementById("signInButton").remove()
+    })
+    .catch((err)=>{
+      console.log(err.code + err.message)
+    })
+  })
+
+// Sign In
+signInButton.addEventListener('click', ()=>{
+    const aemail = email.value
+    const apassword = password.value
+    signInWithEmailAndPassword(auth, aemail, apassword)
+    .then(cred =>{
+      console.log('user logged in:', cred.user)
+      //
+      document.getElementById("email").remove()
+      document.getElementById("password").remove()
+      document.getElementById("signUpButton").remove()
+      document.getElementById("signInButton").remove()
+    })
+  })
+
+// Sign Out
+const logoutButton = document.querySelector('.logout')
+logoutButton.addEventListener('click', () => {
+  signOut(auth)
+    .then(() => {
+      console.log('user signed out')
+    })
+    .catch(err => {
+      console.log(err.message)
+    })
+})
+
+// subscribing to auth changes
+const unsubAuth = onAuthStateChanged(auth, (user) => {
+    console.log('user status changed:', user)
+  })
