e3913c75f5214bac0b92f437381f0560cf38af92
Cris <duqueorange+github@gmail.com>
2024-02-05 20:00:14 -0500

    Sign Up, Sign In, Sign Out are working!

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import { firebaseinit } from './firebasecreds.js';
 firebaseinit();
 
+// Authentication (The auth)
+import{     
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged
+}
+from 'firebase/auth'
+const auth = getAuth()
+
+// Listeners
 const email = document.querySelector('#email')
 const password = document.querySelector('#password')
 const signUpButton = document.querySelector('#signUpButton')
 const signInButton = document.querySelector('#signInButton')
+const signOutButton = document.querySelector('#signOutButton')
 
 // Sign Up
 signUpButton.addEventListener('click', ()=>{
@@ -13,35 +26,39 @@ signUpButton.addEventListener('click', ()=>{
     createUserWithEmailAndPassword(auth,aemail,apassword)
     .then((cred)=>{
       console.log(cred.user)
-      // RESUME: Hide\Show\Sh
-      document.getElementById("email").remove()
-      document.getElementById("password").remove()
-      document.getElementById("signUpButton").remove()
-      document.getElementById("signInButton").remove()
+      // RESUME: Hide and Show
+      document.getElementById('email').remove()
+      document.getElementById('password').remove()
+      document.getElementById('signUpButton').remove()
+      document.getElementById('signInButton').remove()
+
+      document.getElementById('loggedIn').style.display = 'block'
+      // NEXT: Warning if email already in database
     })
     .catch((err)=>{
       console.log(err.code + err.message)
     })
   })
-
-// Sign In
-signInButton.addEventListener('click', ()=>{
+  
+  // Sign In
+  signInButton.addEventListener('click', ()=>{
     const aemail = email.value
     const apassword = password.value
     signInWithEmailAndPassword(auth, aemail, apassword)
     .then(cred =>{
       console.log('user logged in:', cred.user)
       //
-      document.getElementById("email").remove()
-      document.getElementById("password").remove()
-      document.getElementById("signUpButton").remove()
-      document.getElementById("signInButton").remove()
+      document.getElementById('email').remove()
+      document.getElementById('password').remove()
+      document.getElementById('signUpButton').remove()
+      document.getElementById('signInButton').remove()      
+      
+      document.getElementById('loggedIn').style.display = 'block'
     })
   })
-
+  
 // Sign Out
-const logoutButton = document.querySelector('.logout')
-logoutButton.addEventListener('click', () => {
+signOutButton.addEventListener('click', () => {
   signOut(auth)
     .then(() => {
       console.log('user signed out')
@@ -51,7 +68,7 @@ logoutButton.addEventListener('click', () => {
     })
 })
 
-// subscribing to auth changes
+// User status checker. NEXT› Hide and Show
 const unsubAuth = onAuthStateChanged(auth, (user) => {
     console.log('user status changed:', user)
   })
