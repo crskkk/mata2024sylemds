5d60e22e0ee3090372d51b30fe7e40f2ebafb412
Cris <duqueorange+github@gmail.com>
2024-02-22 09:01:39 -0500

    Progress on db, NEED to check security rules and tons of other stuff

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
 import { firebaseinit } from './firebasecreds.js';
+import { initializeApp } from 'firebase/app';
 firebaseinit();
 
 // ::::::AUTHENTICATION:::::: (The auth)
@@ -12,19 +12,16 @@ import{
 }
 from 'firebase/auth'
 const auth = getAuth()
-
-// User status checker. NEXT› Be the ONLY Hide and Show controller
-const unsubAuth = onAuthStateChanged(auth, (user) => {
-    console.log(user.email)
-  })
-
-// Listeners
+// HTML Field Listeners
 const email = document.querySelector('#email')
 const password = document.querySelector('#password')
 const signUpButton = document.querySelector('#signUpButton')
 const signInButton = document.querySelector('#signInButton')
 const signOutButton = document.querySelector('#signOutButton')
-
+// User status checker. NEXT› Be the ONLY Hide and Show controller
+const unsubAuth = onAuthStateChanged(auth, (user) => {
+    console.log(user.email)
+  })
 // Sign Up
 signUpButton.addEventListener('click', ()=>{
     const aemail = email.value
@@ -32,23 +29,21 @@ signUpButton.addEventListener('click', ()=>{
     createUserWithEmailAndPassword(auth,aemail,apassword)
     .then((cred)=>{
       console.log('USER CREATED')
-      // RESUME: Hide and Show
+  // RESUME: Hide and Show
       document.getElementById('email').remove()
       document.getElementById('password').remove()
       document.getElementById('signUpButton').remove()
       document.getElementById('signInButton').remove()
-
       document.getElementById('loggedIn').style.display = 'block'
-      // NEXT:
-        // Say hi - loggedInGreeting innerhtml
-        // Warning if email already in database
+    // NEXT:
+      // Say hi - loggedInGreeting innerhtml
+      // Warning if email already in database
     })
     .catch((err)=>{
-      // NEXT: Specific warnings in UX
+    // NEXT: Specific warnings in UX
       console.log(err.code + err.message)
     })
-  })
-  
+  })  
   // Sign In
   signInButton.addEventListener('click', ()=>{
     const aemail = email.value
@@ -56,16 +51,13 @@ signUpButton.addEventListener('click', ()=>{
     signInWithEmailAndPassword(auth, aemail, apassword)
     .then(cred =>{
       console.log('USER LOGGED IN')
-      //
       document.getElementById('email').remove()
       document.getElementById('password').remove()
       document.getElementById('signUpButton').remove()
       document.getElementById('signInButton').remove()      
-      
       document.getElementById('loggedIn').style.display = 'block'
     })
   })
-  
 // Sign Out
 signOutButton.addEventListener('click', () => {
   signOut(auth)
@@ -77,15 +69,13 @@ signOutButton.addEventListener('click', () => {
     })
 })
 
-// NEXT› Unsubscribe from changes?
-
-
 // ::::::FIRESTORE:::::: (The db)
 import {
   getFirestore,
+  collection,
+  getDocs
 } from 'firebase/firestore'
-initializeApp(firebaseConfig)
-const db = getFirestore
+const db = getFirestore()
 // What to store:
 //   Goal     SMART
 //   Pillars  3-4
@@ -95,4 +85,8 @@ const db = getFirestore
 //   Habits   Enlighten your daily life
 //   Sessions Where habits are practices therefore magic happens
 //   Tracking What's been going on?
-//   
+const colRef = collection(db, 'METAS')
+getDocs(colRef)
+  .then((snapshot) => {
+    console.log(snapshot.docs)
+  })
